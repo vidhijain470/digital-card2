dae39da0b1e4c3b24eb26f90104d8b57450bf94a
singhharshitkumar <79098805+vidhijain470@users.noreply.github.com>
2024-10-26 23:59:47 +0530

    node

--- src/DigitalCard.js
+++ src/DigitalCard.js
@@ -88,7 +88,7 @@ const ContactInfo = styled.div`
 const AboutSection = styled.div`
   margin-top: 20px;
   padding: 10px;
-  background-color: #e9e9e9;
+  background-color: #e9e9e9; 
   border-radius: 5px;
   text-align: left;
 `;
